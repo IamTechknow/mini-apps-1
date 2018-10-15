78f2aaa0dbd442c1232524485ae2e662aa649b61
Edmond Lee <techhexium@gmail.com>
2018-10-15 11:50:54 -0700

    Added model, listeners, init and reset functions

--- challenge_1/README.md
+++ challenge_1/README.md
@@ -7,7 +7,6 @@ This is my implementation of Tic-Tac-Toe, which shall meet the following minimum
 * [ ] As a developer, Tic-Tac-Toe uses bit vectors to store data
 * [ ] As a user, a button resets the game
 * [ ] (OPTIONAL) As a developer, Tic-Tac-Toe uses a bitboard to store data
-* [ ] 
 
 ## Implementation details
 Coming soon!
--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,67 @@
+// Game states and constants
+var NO_WINNER = 0;
+var PLAYER1 = 1;
+var PLAYER2 = 2;
+var WIN_VECTOR = 7; // Match any bit vector with all ones
 
+var winState;
+var turnState;
+
+// Bit vectors for each player
+var p1Rows;
+var p2Rows;
+
+// Helper functions for controlling the game model
+var checkRow = function(player, row) {
+  
+};
+
+var checkCol = function(player, col) {
+  
+};
+
+var checkMajorDiagonal = function(player) {
+  
+};
+
+var checkMinorDiagonal = function(player) {
+  
+};
+
+var getGameText = function() {
+  if (winState !== NO_WINNER) {
+    return winState === PLAYER1 ? 'Player 1 wins!' : 'Player 2 wins!';
+  }
+  return turnState === PLAYER1 ? 'Player 1\'s turn' : 'Player 2\'s turn';
+};
+
+// Event listener functions
+var onReset = function(event) {
+  // Reset model
+  winState = NO_WINNER;
+  turnState = PLAYER1;
+
+  p1Rows = [0, 0, 0];
+  p2Rows = [0, 0, 0];
+
+  // Reset view
+  document.getElementById('gameState').textContent = getGameText();
+};
+
+var onGridClick = function(event) {
+  
+};
+
+// Initialization function, attach listeners
+window.addEventListener('DOMContentLoaded', function() {
+  // Find relevant DOM elements, set listeners
+  document.getElementById('resetBtn').addEventListener('click', onReset);
+
+  for (var ele of document.getElementsByClassName('piece')) {
+    ele.addEventListener('click', onGridClick);
+  }
+
+  // Reset the game
+  onReset();
+}, true);
 
