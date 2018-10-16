4a99c30d6d15f6b925e3021c98134740c06881ea
Edmond Lee <techhexium@gmail.com>
2018-10-16 12:02:38 -0700

    Able to submit data to the server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+var express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+var app = express();
+const PORT = 3000;
 
+// Middleware
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, './client')));
 
+app.post('/csv', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).send(req.body);
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on Port ' + PORT)
+});
