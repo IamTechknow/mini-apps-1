dc03f0bef2feba431135292a962a757f2bf6a794
Edmond Lee <techhexium@gmail.com>
2018-10-17 13:23:24 -0700

    Start on form submit handler, add missing views, fix inputs

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,26 +1,28 @@
 // Constants
-const MAIN = 0, STEP_ONE = 1, STEP_TWO = 2, STEP_THREE = 3, REVIEW = 4;
+const MAIN = 0, STEP_ONE = 1, STEP_TWO = 2, STEP_THREE = 3, REVIEW = 4, FINISH = 5;
 
 // React components
 var Account = (props) => (
   <div>
+    <h3>Account Info</h3>
     <div className="form-row">
       <div className="form-group col-md-6">
         <label htmlFor="account">Account Name</label>
-        <input type="text" className="form-control" id="name" name="account" onChange={props.onChange}></input>
+        <input type="text" className="form-control" id="account" name="account" onChange={props.onChange}></input>
       </div>
 
       <div className="form-group col-md-6">
         <label htmlFor="pass">Password</label>
-        <input type="password" className="form-control" id="level" name="pass" onChange={props.onChange}></input>
+        <input type="password" className="form-control" id="pass" name="pass" onChange={props.onChange}></input>
       </div>
     </div>
-    <button className='btn btn-primary' onClick={props.onNext}>Personal Info</button>
+    <button className='btn btn-primary' type="submit">Personal Info</button>
   </div>
 );
 
 var PersonalInfo = (props) => (
   <div>
+    <h3>Personal Info</h3>
     <div className="form-row">
       <div className="form-group col-md-6">
         <label htmlFor="addr1">Address Line 1</label>
@@ -41,15 +43,16 @@ var PersonalInfo = (props) => (
 
       <div className="form-group col-md-3">
         <label htmlFor="state">State</label>
-        <input type="text" className="form-control" id="state" name="state" onChange={props.onChange}></input>
+        <input type="text" className="form-control" id="state" name="state" maxLength="2" onChange={props.onChange}></input>
       </div>
     </div>
-    <button className='btn btn-primary' onClick={props.onNext}>Personal Info</button>
+    <button className='btn btn-primary' type="submit">Payment Info</button>
   </div>
 );
 
 var PaymentInfo = (props) => (
   <div>
+    <h3>Payment Info</h3>
     <div className="form-row">
       <div className="form-group col-md-9">
         <label htmlFor="credit">Credit Card Number</label>
@@ -62,13 +65,21 @@ var PaymentInfo = (props) => (
       </div>
     </div>
     
-    <button className='btn btn-primary' onClick={props.onNext}>Personal Info</button>
+    <button className='btn btn-primary' type="submit">Review</button>
   </div>
 );
 
 var Review = (props) => (
   <div>
     <h3>Checkout Review</h3>
+    <button className='btn btn-primary' onClick={props.onNext}>Checkout</button>
+  </div>
+);
+
+var Finish = (props) => (
+  <div>
+    <h3>Checkout Complete!</h3>
+    <button className='btn btn-primary' onClick={props.onNext}>Checkout Again!</button>
   </div>
 );
 
@@ -90,21 +101,26 @@ class App extends React.Component {
   
   // Submit handler, handles the form each time continue is pressed
   onSubmit(event) {
+    event.preventDefault();
     // Send a fetch for the corresponding table endpoint
     
+    
     // Increment the current view to render the next one
+    this.onNext();
   }
-  
   
   // Generic input handler, uses input name attribute to change state
   onInputChange(event) {
+    let name = event.target.name;
     
+    // Handle specific cases, otherwise just write to state
+    this.setState({ [name] : event.target.value });
   }
   
   // Simply increment the current state and wrap around at the ended to recall render()
   onNext() {
     var newState = this.state.currView + 1;
-    if (newState > REVIEW) {
+    if (newState > FINISH) {
       newState = MAIN;
     }
     
@@ -117,11 +133,12 @@ class App extends React.Component {
     return (
       <div className="container">
         <form onSubmit={this.onSubmit.bind(this)} noValidate>
-          { this.state.currView === MAIN && <HomePage onNext={this.onNext.bind(this)} onNext={this.onNext.bind(this)} /> }
-          { this.state.currView === STEP_ONE && <Account onChange={this.onInputChange.bind(this)} onNext={this.onNext.bind(this)} /> }
-          { this.state.currView === STEP_TWO && <PersonalInfo onChange={this.onInputChange.bind(this)} onNext={this.onNext.bind(this)} /> }
-          { this.state.currView === STEP_THREE && <PaymentInfo onChange={this.onInputChange.bind(this)} onNext={this.onNext.bind(this)} /> }
+          { this.state.currView === MAIN && <HomePage onNext={this.onNext.bind(this)} /> }
+          { this.state.currView === STEP_ONE && <Account onChange={this.onInputChange.bind(this)} /> }
+          { this.state.currView === STEP_TWO && <PersonalInfo onChange={this.onInputChange.bind(this)} /> }
+          { this.state.currView === STEP_THREE && <PaymentInfo onChange={this.onInputChange.bind(this)} /> }
           { this.state.currView === REVIEW && <Review onChange={this.onInputChange.bind(this)} onNext={this.onNext.bind(this)} /> }
+          { this.state.currView === FINISH && <Finish onNext={this.onNext.bind(this)} /> }
         </form>
       </div>
     );
