562e1a1bf3b8f6477c731657c3081bae01beb4b1
Edmond Lee <techhexium@gmail.com>
2018-10-16 17:14:04 -0700

    Create new route for CSV, turn client to baisc SPA

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,21 @@
+// Modify link to reflect latest CSV file
+var createLink = function(url) {
+  var link = document.getElementById('download');
+  link.setAttribute('href', url);
+  link.textContent = 'Download latest CSV file';
+};
 
-
+// Use fetch to make the POST request
+window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('csvForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const data = new FormData(event.target);
+    fetch('csv', {
+      method: 'POST',
+      mode: 'cors',
+      body: data
+    }).then(response => response.json())
+    .then(response => createLink(response.result));
+  });
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -9,6 +9,9 @@ var app = express();
 var parser = new CSVParser();
 const PORT = 3000;
 
+var fileCount = 1;
+var latestData = '';
+
 // Middleware
 app.use(multer().single('text'));
 app.use(express.static(path.join(__dirname, './client')));
@@ -16,10 +19,17 @@ app.use(express.static(path.join(__dirname, './client')));
 app.post('/csv', (req, res, next) => {
   // Acquire submitted JSON. Assume it is valid JSON.
   var json = req.file.buffer.toString();
+  latestData = parser.readJSON(JSON.parse(json));
+  
+  // Send response back as JSON, client will create link
+  res.writeHead(201, {'Content-Type': 'application/json'});
+  res.end('{"result" : "/latest"}');
+});
 
-  // Send response back as CSV
-  res.writeHead(201, {'Content-Type': 'application/force-download','Content-disposition':'attachment; filename=result.csv'});
-  res.end(parser.readJSON(JSON.parse(json)));
+// Create route for the CSV file.
+app.get('/latest', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/force-download','Content-disposition':'attachment; filename=result.csv'});
+  res.end(latestData);
 });
 
 app.listen(PORT, () => {
