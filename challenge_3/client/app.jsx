709c9b7b1cfcee0116d00cf5a88eb17a9316cecf
Edmond Lee <techhexium@gmail.com>
2018-10-17 11:19:30 -0700

    Got babel and React to work, index page served

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,48 @@
+var Account = (props) => (
+  <div>
 
+  </div>
+);
 
+var PersonalInfo = (props) => (
+  <div>
+
+  </div>
+);
+
+var PaymentInfo = (props) => (
+  <div>
+
+  </div>
+);
+
+var Review = (props) => (
+  <div>
+
+  </div>
+);
+
+var HomePage = (props) => (
+  <div>
+
+  </div>
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
