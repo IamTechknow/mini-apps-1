f72ad56a749aea845f5dfa186df49c92e1a69392
Edmond Lee <techhexium@gmail.com>
2018-10-16 23:49:47 -0700

    Start skeleton code/markup for Grocery checkout

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+var express = require('express');
+const path = require('path');
 
+var app = express();
+const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, './public')));
+
+// TODO: Endpoints that will interact with the database
+
+app.listen(PORT, () => {
+  console.log('Listening on Port ' + PORT)
+});
